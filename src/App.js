f8b2aa62d219f706c95dedc14db5a2fcf281e07f
Fidha Rafeeque <fidharafeequen@gmail.com>
2023-08-01 22:47:30 +0400

    added router links

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import "./App.css";
+import Contact from "./Contact";
+import Homepage from "./Homepage";
+import AboutLittleLemon from "./AboutLittleLemon";
+import { Routes, Route, Link } from "react-router-dom";
 import Card from "./Card";
 import Btn from "./Btn";
 import Usestatecomp from "./Usestatecomp";
@@ -8,6 +12,16 @@ function App() {
 
 return (
     <div className="App">
+        <nav>
+      <Link to="/" className="nav-item">Homepage</Link>
+        <Link to="/about" className="nav-item">About Little Lemon</Link>
+        <Link to="/contact" className="nav-item">Contact Little Lemon</Link>
+	  </nav>
+      <Routes> 
+        <Route path="/" element={<Homepage />}></Route>
+        <Route path="/about" element={<AboutLittleLemon />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+      </Routes>
         <h1>Task: Add three Card elements</h1>
         <Card h2="First card's h2" h3="First card's h3" />
         <Card h2="Second card's h2" h3="Second card's h3" />
